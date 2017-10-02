96cf225627e58bd2e94e2ddbd257b9f4a64f38d3
jarnvans <jarnvans@student.ateveldehs.be>
2017-10-02 16:24:54 +0200

    update 02/10/2017 16:24

---
+++ mobdev-1/lectuere-week-2/js/itunes.js
@@ -0,0 +1,30 @@
+function getJSONByCallbacks(url, succesHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+        if(xhr.status == 200) {
+            var data = (!xhr.responseType)?JSON.parse(xhr.response):xhr.response;
+            succesHandler && succesHandler(data)
+        }
+        else {
+            errorHandler && errorHandler(`Error:${xhr.status}`);
+        }
+    }
+    xhr.onerror = function() {
+        errorHandler && errorHandler('Network Error!');
+    }
+    xhr.send(null);
+}
+
+getJSONByCallbacks('https://itunes.apple.com/search?term=sabaton&entity=album',
+function(data){
+    console.log(data);
+    var tempStr
+    data.forEach(function(album) {
+        
+    }, this);
+},
+function(error) {
+    console.log(error);
+});
